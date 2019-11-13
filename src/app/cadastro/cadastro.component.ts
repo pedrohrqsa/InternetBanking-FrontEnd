5b4ede4e173c0e33c847722de7e285db7a4d0cfb
Luiz Eduardo de Oliveira Martins Leal <48124889+LuizEduOML@users.noreply.github.com>
2019-11-13 11:47:15 -0300

    Atualizando o projeto conforme o cadastro de 12/11

--- src/app/cadastro/cadastro.component.ts
+++ src/app/cadastro/cadastro.component.ts
@@ -1,9 +1,10 @@
 import {Component, OnInit} from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 import {STEPPER_GLOBAL_OPTIONS} from '@angular/cdk/stepper';
+import { Router } from '@angular/router';
+
 import { Cliente, Endereco, Contato, Familiares, ClienteLogin } from './cadastroCliente';
 import { CadastroService } from './cadastro.service';
-import { Router } from '@angular/router';
 
 @Component({
     selector: 'app-cadastro',
@@ -15,10 +16,10 @@ import { Router } from '@angular/router';
 })
 export class CadastroComponent implements OnInit {
   dadosPessoaisFormGroup: FormGroup;
-  FamiliaresFormGroup: FormGroup;
+  infosFamiliaresFormGroup: FormGroup;
   contatoFormGroup: FormGroup;
   enderecoFormGroup: FormGroup;
-  LoginFormGroup: FormGroup;
+  confirmacaoFormGroup: FormGroup;
 
   constructor(private _formBuilder: FormBuilder, private servico: CadastroService, private router: Router) {}
 
@@ -26,50 +27,74 @@ export class CadastroComponent implements OnInit {
     this.dadosPessoaisFormGroup = this._formBuilder.group({
       nome: ['', [Validators.required, Validators.maxLength(40)]],
       sobrenome: ['', [Validators.required, Validators.maxLength(50)]],
-      cpf: ['', [Validators.required, Validators.minLength(11), Validators.pattern(/^-?(0|[1-9]\d*)?$/)]],
-      rg: ['', [Validators.required, Validators.minLength(9), Validators.pattern(/^-?(0|[1-9]\d*)?$/)]],
-      orgaoemissor: ['', [Validators.required]],
-      DtNascimento: ['', [Validators.required]],
-      nacionalidade: ['', [Validators.required]],
-      naturalidade: ['', [Validators.required]]
+      cpf: ['', [Validators.required, Validators.minLength(11), Validators.maxLength(11), Validators.pattern(/^-?(0|[1-9]\d*)?$/)]],
+      rg: ['', [Validators.required, Validators.minLength(9), Validators.maxLength(9), Validators.pattern(/^-?(0|[1-9]\d*)?$/)]],
+      orgaoRg: ['', [Validators.required, Validators.minLength(5), Validators.maxLength(5)]],
+      dataNasc: ['', [Validators.required]],
+      nacionalidade: ['', [Validators.required, Validators.maxLength(20)]],
+      naturalidade: ['', [Validators.required, Validators.maxLength(20)]]
     });
-    this.FamiliaresFormGroup = this._formBuilder.group({
-      nome_Mae: ['', [Validators.required]],
-      sobrenome_Mae: ['', [Validators.required]],
-      nome_Pai: [''],
-      sobrenome_Pai: ['']
+    this.infosFamiliaresFormGroup = this._formBuilder.group({
+      nomeMae: ['', [Validators.required, Validators.maxLength(40)]],
+      sobrenomeMae: ['', [Validators.required, Validators.maxLength(50)]],
+      nomePai: ['', [Validators.maxLength(40)]],
+      sobrenomePai: ['', [Validators.maxLength(50)]]
     });
     this.contatoFormGroup = this._formBuilder.group({
       email:['', [Validators.required, Validators.email]],
-      tel_Resid: [''],
-      tel_Cel: ['', [Validators.required, Validators.pattern(/^-?(0|[1-9]\d*)?$/), Validators.minLength(9), Validators.maxLength(9)]]
+      telefoneResid: ['', [Validators.required, Validators.minLength(8), Validators.maxLength(10), Validators.pattern(/^-?(0|[1-9]\d*)?$/)]],
+      telefoneCel: ['', [Validators.required, Validators.minLength(9), Validators.maxLength(11), Validators.pattern(/^-?(0|[1-9]\d*)?$/)]],
+      proprietario: ['']
     });
     this.enderecoFormGroup = this._formBuilder.group({
-      logradouro: ['', [Validators.required]],
-      numero: ['', [Validators.required]],
-      complemento: [''],
-      bairro: ['', [Validators.required]],
-      cidade: ['', [Validators.required]],
-      sigla_Estado: ['', [Validators.required]],
-      cep: ['', [Validators.required]]
+      logradouro: ['', [Validators.required, Validators.maxLength(50)]],
+      numero: ['', [Validators.required, Validators.pattern(/^-?(0|[1-9]\d*)?$/)]],
+      complemento: ['', [Validators.maxLength(30)]],
+      bairro: ['', [Validators.required, Validators.maxLength(20)]],
+      cidade: ['', [Validators.required, Validators.maxLength(30)]],
+      siglaEstado: ['', [Validators.required, Validators.maxLength(2)]],
+      cep: ['', [Validators.required, Validators.minLength(8), Validators.maxLength(8), Validators.pattern(/^-?(0|[1-9]\d*)?$/)]]
     });
-    this.LoginFormGroup = this._formBuilder.group({
-      cpf: ['', [Validators.required]],
-      senha: ['', [Validators.required]]
+    this.confirmacaoFormGroup = this._formBuilder.group({
+      cpf: ['', [Validators.required, Validators.minLength(11), Validators.maxLength(11), Validators.pattern(/^-?(0|[1-9]\d*)?$/)]],
+      senha: ['', [Validators.required, Validators.minLength(8), Validators.maxLength(15)]]
     })
   }
 
   onCadastro() {
-    const cliente = this.dadosPessoaisFormGroup.getRawValue() as Cliente;
-    const familiares = this.FamiliaresFormGroup.getRawValue() as Familiares;
-    const contato = this.contatoFormGroup.getRawValue() as Contato;
-    const endereco = this.enderecoFormGroup.getRawValue() as Endereco;
-    const clienteLogin = this.LoginFormGroup.getRawValue() as ClienteLogin;
+    const novoCliente = this.dadosPessoaisFormGroup.getRawValue() as Cliente;
+    const familiaresUsuario = this.infosFamiliaresFormGroup.getRawValue() as Familiares;
+    const contatoUsuario = this.contatoFormGroup.getRawValue() as Contato;
+    const enderecoUsuario = this.enderecoFormGroup.getRawValue() as Endereco;
+    const confirmacaoUsuario = this.confirmacaoFormGroup.getRawValue() as ClienteLogin;
+    
     this.servico
-      .cadastro(cliente, familiares, contato, endereco, clienteLogin)
-      .subscribe( 
+      .cadastro(novoCliente, familiaresUsuario, contatoUsuario, enderecoUsuario, confirmacaoUsuario)
+      .subscribe(
         () => this.router.navigate(['']),
         err => console.log(err)
       );
+  }
+
+  numberOnly(event): boolean {
+    const charCode = (event.which) ? event.which : event.keyCode;
+    if (charCode > 31 && (charCode < 48 || charCode > 57)) {
+      return false;
+    }
+    return true;
+  }
+
+  letterOnly(event): boolean {
+    const charCode = (event.which) ? event.which : event.keyCode;
+    if (charCode > 31 && (charCode < 65 || charCode > 90) && (charCode < 97 || charCode > 122) &&
+    charCode != 32 && (charCode < 128 || charCode > 144) && (charCode < 147 || charCode > 154) &&
+    (charCode < 160 || charCode > 165) && (charCode < 181 || charCode > 183) &&
+    (charCode < 198 || charCode > 199) && (charCode < 97 || charCode > 122) &&
+    (charCode < 210 || charCode > 216) && charCode != 222 && charCode != 224 &&
+    (charCode < 226 || charCode > 229) && (charCode < 233 || charCode > 237) &&
+    charCode != 96 && charCode != 126 && charCode != 239) {
+      return false;
+    }
+    return true;
   }
 }
