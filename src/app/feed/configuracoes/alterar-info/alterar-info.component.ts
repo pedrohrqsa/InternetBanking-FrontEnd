218754434d3dff4eb1e676236fee42f5efcbdb9a
luizmarinelli <54442912+luizmarinelli@users.noreply.github.com>
2019-12-19 15:58:58 -0300

    pedro caminhoneiro, leva ferro

--- src/app/feed/configuracoes/alterar-info/alterar-info.component.ts
+++ src/app/feed/configuracoes/alterar-info/alterar-info.component.ts
@@ -6,6 +6,7 @@ import { Cliente } from 'src/app/Models/Cliente';
 import { Familiares } from 'src/app/Models/Familiares';
 import { Contato } from 'src/app/Models/Contato';
 import { Endereco } from 'src/app/Models/Endereco';
+import { InfoContaService } from '../../infos-conta/Infos-conta.service';
 
 @Component({
   selector: 'app-alterar-info',
@@ -14,35 +15,40 @@ import { Endereco } from 'src/app/Models/Endereco';
 })
 export class AlterarInfoComponent implements OnInit {
 
-  indexCPF: number;
-
-  cliente: Cliente;
-  familiares: Familiares;
-  contato: Contato;
-  endereco: Endereco;
-
   editarPerfil: boolean = false;
   editarFamiliares: boolean = false;
   editarContato: boolean = false;
   editarEndereco: boolean = false;
 
+  indexCPF: number;
+  cliente: Cliente;
+  familiares: Familiares;
+  contato: Contato;
+  endereco: Endereco;
+
+  nome: string;
+  sobrenome: string;
+  cpf: string;
+  rg: string;
+  orgaoEmissor: string;
+  dtNascimento: string;
+  nacionalidade: string;
+  naturalidade: string;
+
   constructor(
     private activatedRoute: ActivatedRoute,
-    private alterarInfoService: AlterarInfoService
+    private infoContaService: InfoContaService
   ) { }
 
   ngOnInit() {
     this.getIndexCPF();
     this.onInfoCliente();
-    this.onInfoFamiliares();
-    this.onInfoContato();
-    this.onInfoEndereco();
   }
 
   getIndexCPF() {
     const getCpf = this.activatedRoute.snapshot.paramMap.get('cpf');
 
-    return this.alterarInfoService.getInfoCliente()
+    return this.infoContaService.getInfoCliente()
       .subscribe(clientex =>
         console.log(getCpf,
           this.indexCPF = clientex.findIndex(obj =>
@@ -52,30 +58,17 @@ export class AlterarInfoComponent implements OnInit {
   }
 
   onInfoCliente() {
-    return this.alterarInfoService.getInfoCliente()
-      .subscribe(clientex =>
-        this.cliente = clientex[this.indexCPF],
-      );
-  }
-
-  onInfoFamiliares() {
-    return this.alterarInfoService.getInfoFamiliares()
-      .subscribe(clientex =>
-        this.familiares = clientex[this.indexCPF],
-      );
-  }
-
-  onInfoContato() {
-    return this.alterarInfoService.getInfoContato()
-      .subscribe(clientex =>
-        this.contato = clientex[this.indexCPF],
-      );
-  }
-
-  onInfoEndereco() {
-    return this.alterarInfoService.getInfoEndereco()
-      .subscribe(clientex =>
-        this.endereco = clientex[this.indexCPF],
+    return this.infoContaService.getInfoCliente()
+      .subscribe(clientex => {
+          this.nome = clientex[this.indexCPF].nome;
+          this.sobrenome = clientex[this.indexCPF].sobrenome;
+          this.cpf = clientex[this.indexCPF].cpf;
+          this.rg = clientex[this.indexCPF].rg;
+          this.orgaoEmissor = clientex[this.indexCPF].orgaoEmissor;
+          this.dtNascimento = clientex[this.indexCPF].dtNascimento.toString().substring(0, 10);
+          this.nacionalidade = clientex[this.indexCPF].nacionalidade;
+          this.naturalidade = clientex[this.indexCPF].naturalidade;
+        },
       );
   }
 
