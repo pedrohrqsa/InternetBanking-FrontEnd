e6bb29fb64cd620db6bee1a24a1d4f833111e608
Pedro <pedrohrq22>
2019-12-26 18:58:18 -0300

    CORREÇÃO HTML INFOSCONTA , SERVICE SENHA E RELOAD AO ALTERAR

--- src/app/feed/configuracoes/alterar-senha/alterar-senha.service.ts
+++ src/app/feed/configuracoes/alterar-senha/alterar-senha.service.ts
@@ -1,9 +1,29 @@
 import { Injectable } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { ClienteLogin } from 'src/app/Models/ClienteLogin';
+import { environment } from 'src/environments/environment';
+
+const API = environment.API_URL;
 
 @Injectable({
   providedIn: 'root'
 })
 export class AlterarSenhaService {
 
-  constructor() { }
+  constructor(
+    private http: HttpClient,
+    private activatedRoute: ActivatedRoute
+  ) { }
+
+  url = "http://localhost:5000/api/";
+
+  alterarSenha(cpf: string, senha: ClienteLogin) {
+
+    const apiClienteLogin = '/api/clienteLogin/' + cpf;
+
+    return this
+      .http.put(API + apiClienteLogin, senha,
+        { headers: { 'Content-Type': 'application/json' } })
+  }
 }
